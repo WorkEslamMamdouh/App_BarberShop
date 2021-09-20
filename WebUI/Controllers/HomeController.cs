a272aedda14f0c9f7beb9569a86e22e085cfb46f
WorkEslamMamdouh <81823203+WorkEslamMamdouh@users.noreply.github.com>
2021-09-20 03:18:53 +0200

    Eslam...
    Eslam...

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View("~/Views/Home/Home.cshtml");
+        }
+
+    }
+}
